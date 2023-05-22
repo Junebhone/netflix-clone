c550bbf201849683e4421148bea924c0d475251e
Junebhone <bhonekyawsihein@gmail.com>
2023-05-22 17:32:38 +0630

    project initilization

---
+++ pages/auth.tsx
@@ -0,0 +1,14 @@
+const Auth = () => {
+  return (
+    <div className="relative w-full h-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="w-full h-full bg-black lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="logo" className="h-12" />
+        </nav>
+        <div className="flex "></div>
+      </div>
+    </div>
+  )
+}
+
+export default Auth
---
+++ pages/test.tsx
@@ -0,0 +1,1 @@
+export const APP_VER = "v1.0"
